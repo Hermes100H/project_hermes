c5f66ca000e6bb65395fa223c6b39d33cd7269c6
Erwan TOUX <erwan.toux@mobile2-20.cpe.lan>
2024-01-08 15:18:27 +0100

    creating circuit

---
+++ circuit/generate_circuit.py
@@ -0,0 +1,16 @@
+import numpy as np
+from numpy import poly1d
+
+
+
+
+
+def compute_coord(x_position: float, circuit_function: callable) -> vec2:
+    return vec2(x_position, circuit_function(x_position))
+
+
+def generate_initial_circuit() -> poly1d:
+
+    poly_coeffs = [-0.08, 0.02, 0.3, 0.9, -0.6]
+    return np.poly1d(poly_coeffs)
+
---
+++ circuit/maths_utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import scipy.integrate as itg
+from math import comb
+from numpy import poly1d
+
+
+class vec2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def compute_length(poly_function: np.poly1d, a: float, b: float) -> float:
+    assert a <= b, "a must be lesser than b when computing distance between a and b"
+
+    def df_dx(x):
+        return np.sqrt(1 + poly_function.deriv()(x) ** 2)
+
+    return itg.quad(df_dx, a, b)[0]
+
+
+
+
+def bezier_function(*control_points: vec2):
+
+    B_x = np.poly1d([0])
+    B_y = np.poly1d([0])
+    N = len(control_points)-1
+    for i, control_point in enumerate(control_points):
+        coeff_x = comb(N,i)*control_point.x
+        coeff_y = comb(N,i)*control_point.y
+        B_right = poly1d([1] + [0 for k in range(i)])
+        B_left = poly1d([1 for k in range(N-i)], r=True)
+        B = B_left*B_right
+        print("----------")
+        print(B_left)
+        print(B_right)
+        B_x += coeff_x*B
+        B_y += coeff_y*B
+
+    return B_x, B_y
+
+x,y = bezier_function(vec2(0,0), vec2(1,1))
+print(x(0), x(1))
+print(y(0), y(1))
+a=1
